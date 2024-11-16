a4cfbfd0000271d59819ffb65b5b22fba0376947
aoik109 <allison.ko.family@gmail.com>
2024-11-16 12:41:15 -0800

    Finished Tutorial

---
+++ bro-code-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+
+function App() {
+
+    return (
+        <>
+            <Header></Header>
+            <hr></hr>
+            <Food></Food>
+            <Food></Food>
+            <hr></hr>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default App
